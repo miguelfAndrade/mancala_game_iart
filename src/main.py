605d750a3c43933ff84fc992d708e221e68d7fb2
miguelfAndrade <miguelandrade.96.18@gmail.com>
2019-05-21 10:06:35 +0100

    comand line game logic

--- src/main.py
+++ src/main.py
@@ -12,12 +12,9 @@ def player_vs_player(board):
     mancala.print_board_cmd_line(board)
     player = 0
     winner = 0
-    teste = []
     while(not mancala.game_over(board)):
         print("Player " + str(player + 1) + " turn!")
         teste = ai.playable_pos_list(board, player)
-        print("BEST POS: " + str(ai.pick_best_pos(board, player)))
-        print(teste)
         try:
             piece = mancala.parse_input(int(input("Choose your Piece: ")))
         except Exception:
@@ -41,7 +38,7 @@ def player_vs_player(board):
 
 
 
-def player_vs_pc(board):
+def player_vs_pc(board, level):
     mancala.print_board_cmd_line(board)
     player = 0
     winner = 0
@@ -60,9 +57,7 @@ def player_vs_pc(board):
             mancala.print_board_cmd_line(board)
         elif(player == 1):
             print("Player " + str(player + 1) + " turn!")
-            # piece = ai.random_number_gen(7,12)
-            # piece = ai.pick_best_pos(board, player)
-            piece, minimax_score = ai.minimax(board, board, 1, True, player)
+            piece, minimax_score = ai.minimax_alpha_beta(board, None, level, -math.inf, math.inf, True, player)
             print("CHOOSEN POS: " + str(piece))
             if(not mancala.verify_move(piece, player, board)):
                 continue
@@ -82,7 +77,7 @@ def player_vs_pc(board):
         print("It's a draw!!")
 
 
-def pc_vs_pc(board):
+def pc_vs_pc(board, level1, level2):
     mancala.print_board_cmd_line(board)
     player = 0
     winner = 0
@@ -90,8 +85,7 @@ def pc_vs_pc(board):
         if(player == 0):
             print("Player 1 turn!")
             print(player)
-            piece, minimax_score = ai.minimax(board, None, 5, True, player)
-            # piece, minimax_score = ai.minimax_alpha_beta(board, None, 5, -math.inf, math.inf, True, player)
+            piece, minimax_score = ai.minimax_alpha_beta(board, None, level1, -math.inf, math.inf, True, player)
             print("Player 1 choose: " + str(piece+1))
             # print("minimax score: " + str(minimax_score))
             if(not mancala.verify_move(piece, player, board)):
@@ -100,8 +94,7 @@ def pc_vs_pc(board):
             mancala.print_board_cmd_line(board)
         elif(player == 1):
             print("Player 2 turn!")
-            # piece = ai.random_number_gen(board, player)
-            piece, minimax_score_2 = ai.minimax(board, None, 1, True, player)
+            piece, minimax_score = ai.minimax_alpha_beta(board, None, level2, -math.inf, math.inf, True, player)
             print("Player 2 choose: " + str(piece))
             # print("minimax score: " + str(minimax_score_2))
             if(not mancala.verify_move(piece, player, board)):
@@ -112,8 +105,6 @@ def pc_vs_pc(board):
 
     
     winner = mancala.player_win(board)
-    print("minimax 1 score: " + str(minimax_score))
-    # print("minimax 2 score: " + str(minimax_score_2))
     print("GAME OVER")
     print("Number of seeds:")
     print("Player 1: " + str(board[mancala.PL_1_STORE]))
@@ -127,15 +118,122 @@ def pc_vs_pc(board):
 
 
 def main_cmd_line_game():
+    print("\n\n\n\n\nChoose your mode:\n")
+    print("1 - Player vs Player")
+    print("2 - Player vs PC")
+    print("3 - PC vs PC")
+    while(True):
+        try:
+            user = int(input("Select: "))
+            if(user == 1 or user == 2 or user == 3):
+                break
+            else:
+                print("Choose between 1, 2 and 3!")
+                continue
+        except Exception:
+            print("INVALID INPUT!")
+            continue
+
     board = mancala.init_board(mancala.BOARD_SIZE)
-    # player_vs_player(board)
-    # player_vs_pc(board)
-    pc_vs_pc(board)
+    if(user == 1):
+        player_vs_player(board)
+    elif(user == 2):
+        print("Choose the difficulty level:")
+        print("1 - Easy")
+        print("2 - Medium")
+        print("3 - Hard")
+        while(True):
+            try:
+                level = int(input("Select: "))
+                if(level == 1 or level == 2 or level == 3):
+                    break
+                else:
+                    print("Choose between 1, 2 and 3!")
+                    continue
+            except Exception:
+                print("INVALID INPUT!")
+                continue
+        if(level == 1):
+            player_vs_pc(board, 2)
+        elif(level == 2):
+            player_vs_pc(board, 5)
+        else:
+            player_vs_pc(board, 8)
+    else:
+        while(True):
+            print("Choose the difficulty level for pc 1:")
+            print("1 - Easy")
+            print("2 - Medium")
+            print("3 - Hard")
+            try:
+                level1 = int(input("Select: "))
+                if(level1 == 1 or level1 == 2 or level1 == 3):
+                    break
+                else:
+                    print("Choose between 1, 2 and 3!")
+                    continue
+            except Exception:
+                print("INVALID INPUT!")
+                continue
+
+        while(True):
+            print("Choose the difficulty level for pc 2:")
+            print("1 - Easy")
+            print("2 - Medium")
+            print("3 - Hard")
+            try:
+                level2 = int(input("Select: "))
+                if(level2 == 1 or level2 == 2 or level2 == 3):
+                    break
+                else:
+                    print("Choose between 1, 2 and 3!")
+                    continue
+            except Exception:
+                print("INVALID INPUT!")
+                continue
+        
+        if(level1 == 1):
+            pc_1 = 2
+        elif(level1 == 2):
+            pc_1 = 5
+        else:
+            pc_1 = 8
+
+        if(level2 == 1):
+            pc_2 = 2
+        elif(level2 == 2):
+            pc_2 = 5
+        else:
+            pc_2 = 8
+        
+        pc_vs_pc(board, pc_1, pc_2)
 
 
 
+def choose_cmd_or_gui():
+    print("\n\n\nMANCALA BOARD GAME")
+    print("\nSelect one of the options:")
+    print("1 - Comand line Game")
+    print("2 - Graphical Interface Game")
+    while(True):
+        try:
+            user = int(input("Select: "))
+            if(user == 1 or user == 2):
+                break
+            else:
+                print("Choose between 1 and 2!")
+                continue
+        except Exception:
+            print("INVALID INPUT!")
+            continue
+    if(user == 1):
+        main_cmd_line_game()
+    else:
+        main_gui()
+        # print("Not implemented!")
 
-def main():
+
+def main_gui():
 
     pygame.init()
 
@@ -155,7 +253,7 @@ def main():
                 sys.exit()
             
             if event.type == pygame.MOUSEBUTTONDOWN:
-                player_vs_player(board)
+                print("Hello!!!!")
 
 
-main_cmd_line_game()
+choose_cmd_or_gui()
